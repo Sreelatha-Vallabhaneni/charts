ba8bb9971cd4189fa658d5bf2a277a5971d3c3fe
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-03-10 23:19:02 +0100

    added styling

--- Recharts/src/components/chart.js
+++ Recharts/src/components/chart.js
@@ -9,50 +9,61 @@ import {
 } from "recharts";
 //import sampleData from '../data/sampleData.json';
 import shiso from './lettuce.json';
+import './chart.style.css';
 
 
 const Chart = () => {
   return (
     <div>
       <h2 style={{ textAlign: "left" }}>TEMPERATURE (°C)</h2>
-      <button>
-        <ResponsiveContainer width={"100%"} height={400}>
-          <LineChart
+      <ResponsiveContainer width={"90%"} height={400} className="lineChart">
+        <LineChart
+          data={shiso}
+          margin={{ top: 5, right: 80, left: 20, bottom: 10 }}          
+        >
+          <XAxis
+            stroke="white"
+            dataKey="timestamp"
+            domain={["dataMin", "dataMax"]}
+            //interval={0}
+            tick={{ fontSize: 10, fill: "grey", strokeWidth: 2 }}
+            angle={30}
+          ></XAxis>
+          <YAxis
+            //dataKey="temp"
+            stroke="white"
+            type="number"
+            domain={["dataMin", "dataMax"]}
+            tick={{ fontSize: 10, fill: "grey", strokeWidth: 2 }}
+            // ticks={[0, 5, 10, 15, 20, 25, 30]}
+          ></YAxis>
+          <Tooltip />
+          <ReferenceLine y={17} stroke="red" strokeWidth={1} />
+          <ReferenceLine y={20} stroke="blue" strokeWidth={1} />
+          <ReferenceLine y={23} stroke="red" strokeWidth={1} />
+          <Line
+            dataKey="temp"
+            name="Temperature"
+            unit={"°C"}
+            fill="black"
+            stroke="black"
+            type={"natural"}
+            strokeWidth={2}
+          />
+          <Brush
+            className="brush"
+            startIndex={0}
+            endIndex={100}
+            height={20}
+            dataKey="timeStamp"
             data={shiso}
-            margin={{ top: 5, right: 40, left: 20, bottom: 10 }}
-          >
-            <XAxis
-              stroke="white"
-              dataKey="timestamp"
-              domain={["dataMin", "dataMax"]}
-              //interval={0}
-              tick={{ fontSize: 10, fill: "blue", strokeWidth: 2 }}
-              angle={30}
-            ></XAxis>
-            <YAxis
-              //dataKey="temp"
-              stroke="white"
-              type="number"
-              domain={["dataMin", "dataMax"]}
-              tick={{ fontSize: 10, fill: "blue", strokeWidth: 2 }}
-              // ticks={[0, 5, 10, 15, 20, 25, 30]}
-            ></YAxis>
-            <Tooltip />
-            <ReferenceLine y={17} stroke="green" strokeWidth={2} />
-            <ReferenceLine y={20} stroke="blue" strokeWidth={1} />
-            <ReferenceLine y={24} stroke="red" strokeWidth={1} />
-            <Line
-              dataKey="temp"
-              name="Temperature"
-              unit={"°C"}              
-              fill="blue"
-              type={"natural"}
-              strokeWidth={2}
-            />
-            <Brush />
-          </LineChart>
-        </ResponsiveContainer>
-      </button>
+            stroke="rgb(27, 164, 207)"
+            // fill="rgb(250, 252, 252)"
+            // style={{fontSize:"80%"}}
+            gap={5}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
